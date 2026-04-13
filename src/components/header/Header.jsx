c71eec90a1132dbe101b0fda30ee66bff46a4599
hossidev <nsimba2025nwayami@gmail.com>
2026-04-13 16:57:35 +0100

    Estrutura do inicial do menu feito

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,16 @@ export default function Header() {
 
   return (
     <header>
+      <Link to="/" className="logo">Kamba Delivery</Link>
+
+      <nav className="navbar">
+        <Link to="/">Início</Link>
+        <HashLink to="/#como-funciona">Como Funciona</HashLink>
+        <HashLink to="/#sou-solicitante">Sou Solicitante</HashLink>
+        <HashLink to="/#sou-entregador">Sou Entregador</HashLink>
+      </nav>
+
+      <div className="fas fa-bars-staggered"></div>
     </header>
   );
 }
