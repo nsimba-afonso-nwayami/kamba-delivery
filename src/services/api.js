c2c9942724b81277e867c6f5021250697e13846c
hossidev <nsimba2025nwayami@gmail.com>
2026-04-16 11:10:02 +0100

    Interceptor de token automático adicionado

--- src/services/api.js
+++ src/services/api.js
@@ -8,3 +8,38 @@ export const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// REQUEST INTERCEPTOR (manda token automaticamente)
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refresh = localStorage.getItem("refresh_token");
+
+        const res = await api.post("auth/token/refresh/", {
+          refresh,
+        });
+
+        const newAccess = res.data.access;
+
+        localStorage.setItem("access_token", newAccess);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccess}`;
+
+        return api(originalRequest);
+      } catch (err) {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
