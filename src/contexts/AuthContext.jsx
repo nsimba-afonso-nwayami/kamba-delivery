9c8867e07a9fb851d4928b8ca35f50f3dfada1b2
hossidev <nsimba2025nwayami@gmail.com>
2026-04-16 11:11:48 +0100

    Context de autenticação feito

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { getUsuarioById } from "../services/usuariosService";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const isAuthenticated = !!localStorage.getItem("access_token");
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const token = localStorage.getItem("access_token");
+      const userId = localStorage.getItem("user_id");
+
+      if (!token || !userId) {
+        setUser(null);
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const userData = await getUsuarioById(userId);
+
+        setUser({
+          ...userData,
+          tipo: localStorage.getItem("tipo"),
+        });
+      } catch (error) {
+        console.log("Erro ao carregar usuário:", error);
+
+        localStorage.clear();
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadUser();
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    setUser(null);
+    window.location.href = "/login";
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        loading,
+        isAuthenticated,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
