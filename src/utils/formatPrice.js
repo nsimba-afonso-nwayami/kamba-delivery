e61d52ee6e04015587ecbd0f73ba3dcb7558e41b
hossidev <nsimba2025nwayami@gmail.com>
2026-04-17 12:37:51 +0100

    Função para formatação de moeda criada e feita

---
+++ src/utils/formatPrice.js
@@ -0,0 +1,8 @@
+
+export const formatPrice = (value) => {
+  return new Intl.NumberFormat("pt-AO", {
+    style: "currency",
+    currency: "AOA",
+    maximumFractionDigits: 0,
+  }).format(value);
+};
