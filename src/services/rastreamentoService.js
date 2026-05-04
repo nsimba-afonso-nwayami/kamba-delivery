2fa2cd3eb02035c331d93c9c57ffa260494ac4ca
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-04 12:51:52 +0100

    Service atualizado com a função de pegar a posição do entregador

--- src/services/rastreamentoService.js
+++ src/services/rastreamentoService.js
@@ -29,3 +29,43 @@ export const enviarPosicao = async (pedidoId, latitude, longitude) => {
     console.error("Erro rastreamento:", err);
   }
 };
+
+// buscar última posição do entregador
+export const getUltimaPosicaoEntregador = async (pedidoId) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await fetch(
+      `${BASE_URL}/rastreamento/pedidos/${pedidoId}/posicoes/`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          ...(token && {
+            Authorization: `Token ${token}`,
+          }),
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Erro ao buscar posição");
+    }
+
+    const data = await response.json();
+
+    if (Array.isArray(data) && data.length > 0) {
+      const ultima = data[0];
+
+      return [
+        parseFloat(ultima.latitude),
+        parseFloat(ultima.longitude),
+      ];
+    }
+
+    return null;
+  } catch (err) {
+    console.log("Erro rastreamento:", err);
+    return null;
+  }
+};
